699086e6f05ddca94dcf46b5f223c3c32097cd40
rahma <fijriatirahmaturrizqi@gmail.com>
2023-05-09 14:44:14 +0700

    membuat hello world di asp.net mvc

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*public string Index()
+        {
+            return "Selamat datang pada praktikum .net";
+        }*/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
